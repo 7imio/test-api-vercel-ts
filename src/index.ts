09ac88927d4d0618f7097674fc776e5a4532df27
anthony deplanque <anthony.deplanque@csgroup.eu>
2025-02-24 13:18:12 +0100

    test without port

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
-
 app.get('/', (req: Request, res: Response) => {
   res.send('API is running');
 });
